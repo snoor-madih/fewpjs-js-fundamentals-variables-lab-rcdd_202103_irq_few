e538fe41f0f98692204c1a80cea74d4f1360668f
snoor_madih <snoor.madih@gmail.com>
2021-05-12 11:41:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Code your solution in this file!
 // let companyName = 'Scuber';
+<<<<<<< HEAD
 const companyName = 'Scuber';
 let mostProfitableNeighborhood ='Chelsea';
 let companyCeo = 'Susan Smith';
+=======
+const companyName = 'Scuber';
+>>>>>>> 25e86b2032523a45277cf3c7cfaa0c96778b5bde
